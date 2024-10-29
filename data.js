40c764d0e69d4d973957ca47bb982767ae857979
WesleySherman <wesley.sherman@expsouthafrica.co.za>
2024-10-29 11:26:24 +0200

    Commit

---
+++ data.js
@@ -0,0 +1,695 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01---front-door-2",
+      "name": "01 - Front Door 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 480,
+      "initialViewParameters": {
+        "yaw": -2.5748579272976926,
+        "pitch": 0.10160685918573797,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3709588555317431,
+          "pitch": 0.3577428972442771,
+          "rotation": 0,
+          "target": "1-02---mid-staircase"
+        },
+        {
+          "yaw": -2.2845515159116943,
+          "pitch": 0.21211215743453415,
+          "rotation": 0,
+          "target": "4-05---back-of-office"
+        },
+        {
+          "yaw": 2.662022691498752,
+          "pitch": 0.2196058182477163,
+          "rotation": 0,
+          "target": "3-04---platform-office"
+        },
+        {
+          "yaw": -1.5814470074030194,
+          "pitch": 0.19008132821443624,
+          "rotation": 0,
+          "target": "6-07---bar-area"
+        },
+        {
+          "yaw": -0.8565126847602631,
+          "pitch": 0.367978511338622,
+          "rotation": 0,
+          "target": "5-06---small-office"
+        },
+        {
+          "yaw": -1.981344851973141,
+          "pitch": 0.07380086768737293,
+          "rotation": 0,
+          "target": "7-08---walled-office"
+        },
+        {
+          "yaw": -2.4033214717516422,
+          "pitch": 0.10929392314746522,
+          "rotation": 0,
+          "target": "9-10---fire-escape"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02---mid-staircase",
+      "name": "02 - Mid Staircase",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.190435731564099,
+        "pitch": -0.10978353553388231,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.11024203274971711,
+          "pitch": -0.10279281529552797,
+          "rotation": 0,
+          "target": "0-01---front-door-2"
+        },
+        {
+          "yaw": 2.7186576354515193,
+          "pitch": 0.13992590221419654,
+          "rotation": 0,
+          "target": "2-03---parking-lot"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03---parking-lot",
+      "name": "03 - Parking Lot",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.41351569492604767,
+        "pitch": 0.08644944102488772,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6019117126839753,
+          "pitch": 0.023922156038809206,
+          "rotation": 0,
+          "target": "1-02---mid-staircase"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-04---platform-office",
+      "name": "04 - Platform Office",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.7131212232438813,
+        "pitch": -0.009398784133836102,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.30747271929015163,
+          "pitch": 0.2487136723450316,
+          "rotation": 0,
+          "target": "0-01---front-door-2"
+        },
+        {
+          "yaw": -1.5121004845871084,
+          "pitch": 0.22218592791240077,
+          "rotation": 0,
+          "target": "4-05---back-of-office"
+        },
+        {
+          "yaw": -0.6755053445137893,
+          "pitch": 0.1619994668657867,
+          "rotation": 0,
+          "target": "5-06---small-office"
+        },
+        {
+          "yaw": -1.0405985891845901,
+          "pitch": 0.12326355683803492,
+          "rotation": 0,
+          "target": "6-07---bar-area"
+        },
+        {
+          "yaw": -1.5423372358622363,
+          "pitch": 0.08081202228715689,
+          "rotation": 0,
+          "target": "7-08---walled-office"
+        },
+        {
+          "yaw": -1.880190284680662,
+          "pitch": 0.14175952220871757,
+          "rotation": 4.71238898038469,
+          "target": "9-10---fire-escape"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-05---back-of-office",
+      "name": "05 - Back of Office",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.5094331606009792,
+        "pitch": 0.046313937323574805,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.4859479304451497,
+          "pitch": 0.1658771149562348,
+          "rotation": 0,
+          "target": "3-04---platform-office"
+        },
+        {
+          "yaw": -0.5215235394071289,
+          "pitch": 0.19079971169842302,
+          "rotation": 0,
+          "target": "0-01---front-door-2"
+        },
+        {
+          "yaw": -1.0362419142031545,
+          "pitch": 0.19352109765533498,
+          "rotation": 0,
+          "target": "5-06---small-office"
+        },
+        {
+          "yaw": 2.49717196266797,
+          "pitch": 0.2191158583479833,
+          "rotation": 0,
+          "target": "9-10---fire-escape"
+        },
+        {
+          "yaw": 2.7441765590493574,
+          "pitch": 0.22284854604704485,
+          "rotation": 0,
+          "target": "8-09---bathroom"
+        },
+        {
+          "yaw": -2.8226323865946803,
+          "pitch": 0.1917162762303839,
+          "rotation": 0,
+          "target": "7-08---walled-office"
+        },
+        {
+          "yaw": -1.6981591183982907,
+          "pitch": 0.2641676145155216,
+          "rotation": 0,
+          "target": "6-07---bar-area"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-06---small-office",
+      "name": "06 - Small Office",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 2.304758150253944,
+        "pitch": -0.006789014540627747,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.5550448311559473,
+          "pitch": 0.22233032967645983,
+          "rotation": 6.283185307179586,
+          "target": "4-05---back-of-office"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-07---bar-area",
+      "name": "07 - Bar Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.8649260745489755,
+        "pitch": -0.02814300501934497,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.141556547780368,
+          "pitch": 0.33464260604683815,
+          "rotation": 0,
+          "target": "4-05---back-of-office"
+        },
+        {
+          "yaw": 2.435603723288132,
+          "pitch": 0.14162451804408605,
+          "rotation": 0,
+          "target": "3-04---platform-office"
+        },
+        {
+          "yaw": 1.4645911910667602,
+          "pitch": 0.4862499743979125,
+          "rotation": 4.71238898038469,
+          "target": "5-06---small-office"
+        },
+        {
+          "yaw": -2.215440523648244,
+          "pitch": 0.1504470796233477,
+          "rotation": 1.5707963267948966,
+          "target": "7-08---walled-office"
+        },
+        {
+          "yaw": 1.8519554612379467,
+          "pitch": 0.27074772636542654,
+          "rotation": 0,
+          "target": "0-01---front-door-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-08---walled-office",
+      "name": "08 - Walled Office",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.499679383204823,
+        "pitch": -0.011278540960603323,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1950455825893869,
+          "pitch": 0.2321312879868671,
+          "rotation": 0,
+          "target": "4-05---back-of-office"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-09---bathroom",
+      "name": "09 - Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.1617045360122678,
+        "pitch": 0.26316595574741086,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2742333163256383,
+          "pitch": 0.2540336307829314,
+          "rotation": 0,
+          "target": "4-05---back-of-office"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-10---fire-escape",
+      "name": "10 - Fire Escape",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.18761290883398196,
+        "pitch": 0.08226635070655419,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5987130426867537,
+          "pitch": 0.22353611304689736,
+          "rotation": 0,
+          "target": "4-05---back-of-office"
+        },
+        {
+          "yaw": 1.101798884277681,
+          "pitch": 0.3470539726568802,
+          "rotation": 0,
+          "target": "10-11---kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-11---kitchen",
+      "name": "11 - Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.3325602740161617,
+        "pitch": 0.0911974716170203,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.005876130022111,
+          "pitch": 0.3502500177865926,
+          "rotation": 0,
+          "target": "9-10---fire-escape"
+        },
+        {
+          "yaw": -0.770275713262567,
+          "pitch": 0.3992066849609799,
+          "rotation": 0,
+          "target": "11-12---boardroom"
+        },
+        {
+          "yaw": 0.06837947735984073,
+          "pitch": 0.11472500692566534,
+          "rotation": 4.71238898038469,
+          "target": "12-13---storeroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-12---boardroom",
+      "name": "12 - Boardroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.4599846090862236,
+        "pitch": 0.13987712195320512,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1445801646956646,
+          "pitch": 0.4392619925527512,
+          "rotation": 0,
+          "target": "10-11---kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-13---storeroom",
+      "name": "13 - Storeroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.32489975306355845,
+        "pitch": 0.009398784133836102,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.130556032688391,
+          "pitch": 0.30273551982287117,
+          "rotation": 1.5707963267948966,
+          "target": "10-11---kitchen"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Mercado Centre, Shop 10",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
